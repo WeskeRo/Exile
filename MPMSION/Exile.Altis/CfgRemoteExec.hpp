64ea531e53e5c5a6c1c3dc9a130cd177170054e7
WeskeR <wesker01@gmail.com>
2018-11-18 21:31:22 +0100

    adding

---
+++ MPMSION/Exile.Altis/CfgRemoteExec.hpp
@@ -0,0 +1,16 @@
+class CfgRemoteExec
+{
+	class Functions
+	{
+		mode = 1;
+		jip = 0;
+		class fnc_AdminReq { allowedTargets=2; };	// infiSTAR AntiHack
+		class fn_xm8apps_server { allowedTargets=2; };	// infiSTAR xm8apps
+		class ExileServer_system_network_dispatchIncomingMessage { allowedTargets=2; };	// ExileMod
+	};
+	class Commands
+	{
+		mode=0;
+		jip=0;
+	};
+};
